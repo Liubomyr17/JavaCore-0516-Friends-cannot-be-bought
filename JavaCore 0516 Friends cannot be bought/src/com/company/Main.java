171b508bf0b28202f9af97f3c62b10bf43159949
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-08 10:54:54 +0200

    Add files via upload

---
+++ JavaCore 0516 Friends cannot be bought/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Scanner;
+
+/*
+
+
+
+*/
+
+// public class Friend
+public class Main {
+    private String name = null;
+    private int age;
+    private char sex;
+
+      public Main(String name) {
+          this.name = name;
+    }
+
+    public Main(String name, int age) {
+          this.name = name;
+          this.age = age;
+    }
+
+    public Main(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+    
+    public static void main(String[] args) {
+    }
+}
+
+
+
+
